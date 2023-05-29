86148482c149d695c2cce2e4638c8eeb26cb732a
Alexander <alexanderlysenko99@mail.ru>
2023-05-29 12:37:24 +0300

    quiz picture

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,17 @@
 import styles from './main.module.css';
 import { Link } from 'react-router-dom';
 import { Button } from '../../components/ButtonControl/Button';
+import { ReactComponent as BackImg } from '../../assets/img/quizzesBackground.svg';
 
 const Main = () => {
   return (
     <div className={styles.mainWrapper}>
+      <BackImg />
       <Link to={'/quizzes'}>
         {' '}
         <Button text={'Quizes'}></Button>
+        Здесь будет всякая хуйня о сайте
       </Link>
-      Здесь будет всякая хуйня о сайте
     </div>
   );
 };
