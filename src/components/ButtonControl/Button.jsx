66856168e0a4e8e32b0daf0710f4d7d2c2dc18de
Alexander <alexanderlysenko99@mail.ru>
2023-07-02 19:07:28 +0300

    mui styles

--- src/components/ButtonControl/Button.jsx
+++ src/components/ButtonControl/Button.jsx
@@ -1,7 +1,11 @@
 import styles from './button.module.css';
-export const Button = ({ text, onclick }) => {
+export const Button = ({ text, onclick, isDisabled }) => {
   return (
-    <button className={styles.buttonControl} onClick={onclick}>
+    <button
+      className={styles.buttonControl}
+      onClick={onclick}
+      disabled={isDisabled}
+    >
       {text}
     </button>
   );
--- src/components/QuizQuestion/QuizQuestion.jsx
+++ src/components/QuizQuestion/QuizQuestion.jsx
@@ -1,48 +1,119 @@
 import styles from './QuizQuestion.module.css';
-import { Button } from '../ButtonControl/Button';
+// import { Button } from '../ButtonControl/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { Button } from '@mui/material';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import Highlight from 'react-highlight';
 export const QuizQuestion = ({ category, questions }) => {
-  const [questionNumber, setQuestionNumber] = useState(0);
   console.log('array with questions =>>', questions);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [answerColor, setAnswerColor] = useState('white');
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(false);
+  const [helperText, setHelperText] = useState('');
+  const [isDisabled, setDisabled] = useState(false);
 
   const handleNextQuestion = (e, questionNumber) => {
+    setDisabled(false);
+    setError(false);
+    setHelperText('');
     e.preventDefault();
+    setValue('');
     if (questionNumber + 1 < questions.length) {
       setQuestionNumber(questionNumber + 1);
     }
   };
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   const handleAnswer = (e) => {
     e.preventDefault();
-    console.log('handleAnswer');
+    if (Number(value) === questions[questionNumber].correctAnswer) {
+      setError(false);
+      setHelperText('Nice, it is correct answer!');
+      setDisabled(true);
+      setAnswerColor('green');
+    } else {
+      setError(true);
+      setHelperText('Sorry, wrong answer!');
+      setAnswerColor('red');
+      setDisabled(true);
+    }
   };
   if (questions.length === 0) return null;
   return (
     <div className={styles.questionBox}>
-      <Link to={`/quizzes/${category}`}>Назад к выбору теста</Link>
+      <Link to={`/quizzes/${category}`}>
+        <ArrowBackIcon /> Назад к выбору теста
+      </Link>
       <h1
         className={styles.questionBoxTitle}
-      >{`Тест на знание технологии${category}`}</h1>
+      >{`Тест на знание технологии ${category}`}</h1>
       <div>
         <div>тут будет прогресс барр</div>
-        <form>
-          <div className={styles.questionText}>
-            {questions[questionNumber].question}
-          </div>
-          <ul className={styles.answerBox}>
+        <div className={styles.questionText}>
+          {questions[questionNumber].question}
+        </div>
+        <FormControl error={error} disabled={isDisabled} size='medium'>
+          <FormLabel
+            id='demo-controlled-radio-buttons-group'
+            sx={{ fontSize: '30px', color: 'white' }}
+          >
+            Варианты ответа:
+          </FormLabel>
+          <RadioGroup
+            aria-labelledby='demo-controlled-radio-buttons-group'
+            name='controlled-radio-buttons-group'
+            value={value}
+            onChange={handleChange}
+          >
             {questions[questionNumber].answerOptions.map((answer, index) => {
-              return <li key={index}>{answer}</li>;
+              const answerCode = <Highlight>{answer}</Highlight>;
+              return (
+                <FormControlLabel
+                  value={index}
+                  control={<Radio />}
+                  label={answerCode}
+                  key={index}
+                />
+              );
             })}
-          </ul>
-          <div className={styles.questionButtons}>
-            <Button
-              text={'Пропустить вопрос'}
-              onclick={(e) => handleNextQuestion(e, questionNumber)}
-            ></Button>
-            <Button text={'Ответить'} onclick={(e) => handleAnswer(e)}></Button>
-          </div>
-        </form>
+          </RadioGroup>
+          <FormHelperText sx={{ color: `${answerColor}!important` }}>
+            {helperText}
+          </FormHelperText>
+        </FormControl>
+        <div className={styles.btnContainer}>
+          <Button
+            onClick={(e) => handleNextQuestion(e, questionNumber)}
+            variant='outlined'
+            disabled={isDisabled}
+          >
+            Пропустить вопрос
+          </Button>
+          <Button
+            variant='outlined'
+            onClick={(e) => handleNextQuestion(e, questionNumber)}
+          >
+            Следующий вопрос
+          </Button>
+          <Button
+            variant='outlined'
+            onClick={(e) => handleAnswer(e)}
+            disabled={isDisabled}
+          >
+            Ответить
+          </Button>
+        </div>
       </div>
     </div>
   );
