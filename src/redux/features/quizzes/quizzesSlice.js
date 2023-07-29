55368eaa75d6bbf783a4c2c5ae6b12d1236f3590
Mark Sorokin <jetblade0@gmail.com>
2023-07-29 14:58:40 +0300

    2

--- src/redux/features/quizzes/quizzesSlice.js
+++ src/redux/features/quizzes/quizzesSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const loadQuiz = createAsyncThunk(
   "@@quizzes/load-quiz",
   async (name, { extra: { client, api } }) => {
-    return client.get(api.BASE_URL + `?query=${name}`);
+    return client.get("https://2maslenka-quizzes.ru/api" + `?query=${name}`);
   }
 );
 
